09613829c1eafb71386daa68bb5b4041abdebaa4
d1 <www.didowu@gmail.com>
2022-11-08 18:23:35 +0100

    added viewmodel to get the data through the repository and make it available to the mainactivity

--- app/src/main/java/com/example/rickandmortyretrofit/MainViewModel.kt
+++ app/src/main/java/com/example/rickandmortyretrofit/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.rickandmortyretrofit
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.rickandmortyretrofit.network.ApiClient
 import com.example.rickandmortyretrofit.network.Character
+import com.example.rickandmortyretrofit.network.CharacterResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel(private val repository: Repository
                     = Repository(ApiClient.apiService)
@@ -13,4 +18,31 @@ class MainViewModel(private val repository: Repository
     private var _charactersLiveData = MutableLiveData<List<Character>>()
     val characterLiveData: LiveData<List<Character>>
     get() = _charactersLiveData
+
+    init {
+        fetchCharacters()
+    }
+
+    private fun fetchCharacters() {
+        val client = repository.getCharacters("1")
+        client.enqueue( object : Callback<CharacterResponse> {
+            override fun onResponse(
+                call: Call<CharacterResponse>,
+                response: Response<CharacterResponse>
+            ) {
+                 if (response.isSuccessful){
+                     _charactersLiveData.postValue(response.body()?.results)
+
+                 }
+            }
+
+            override fun onFailure(call: Call<CharacterResponse>, t: Throwable) {
+                Log.d("failure",t.message.toString())
+            }
+
+        })
+
+    }
+
+
 }
